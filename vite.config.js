da42b9d73c3b97f432708fd841f7d29df3ab794a
Supanun Chitmeta <supanun.cas@gmail.com>
2026-06-19 14:55:54 +0700

    fix vite base path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/my-react-web/', // <-- เพิ่มบรรทัดนี้ (เปลี่ยนชื่อให้ตรงกับ Repository ของคุณ)
+  base: '/Portfolio-Supanun/', // <-- เพิ่มบรรทัดนี้ (เปลี่ยนชื่อให้ตรงกับ Repository ของคุณ)
 })
